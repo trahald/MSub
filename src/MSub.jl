5afb20cbd99efb75076d4c603b8649d6eb2f2719
Jacupo <Jacupo@users.noreply.github.com>
2020-03-27 16:28:55 +0100

    •sub_monomial(MTermpoly,Monomial,MTermPoly)

--- src/MSub.jl
+++ src/MSub.jl
@@ -9,17 +9,17 @@ const MTermPoly{C,T} = Union{MTermLike{C,T}, Polynomial{C,T}};
 include("Find_monomial.jl");
 include("Sub_monomial.jl");
 
-function Sub2(
+function sub_monomial(
               poly1::Polynomial{C,T},
               mon2::Union{PolyVar{C}, Monomial{C}},
-              mon3::Union{PolyVar{C}, Monomial{C}}) where {C,T}
+              mon3::MTermPoly{C,T}) where {C,T}
     poly = 0;
     for (c, m) in zip(coefficients(poly1), monomials(poly1))
-        poly    += c*Sub_monomial(Monomial{C}(variables(poly1), exponents(m)), mon2, mon3);
+        poly    += c*sub_monomial(Monomial{C}(variables(poly1), exponents(m)), mon2, mon3);
     end
 
     return poly;
 end
 
-export Sub2, Sub_monomial;
+export sub_monomial;
 end
